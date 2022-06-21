6e4f1ced11edcbd8d5a9ded97a8ab5aaf9249c91
EgorKirillov <80296253508@mail.ru>
2022-06-21 18:37:23 +0300

    skeleton of portfolio? some refactoring without @media

--- src/MyWorks/Work/OneWork.tsx
+++ src/MyWorks/Work/OneWork.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import style from "./OneWork.module.css"
 
 
-const OneWork = (props:{title:string; discription:string}) => {
+const OneWork = (props: { title: string; discription: string }) => {
    return (
      <div className={style.mywork}>
-     <div className={style.preview}>
-        <img src="" alt="img"/>
-     </div>
+        <div className={style.preview}>
+           <a href=" "><img src=" " alt="img"/></a>
+        </div>
         <h3>{props.title}</h3>
         <div className={style.description}>{props.discription}</div>
      
