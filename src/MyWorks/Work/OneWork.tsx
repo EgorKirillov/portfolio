717c5f2f1750d71e2365a985f898acdf346ad23d
EgorKirillov <80296253508@mail.ru>
2022-07-26 10:29:02 +0300

    before install sass

--- src/MyWorks/Work/OneWork.tsx
+++ src/MyWorks/Work/OneWork.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import style from "./OneWork.module.css"
 
 
-const OneWork = (props: { title: string; discription: string }) => {
+const OneWork = (props: { style:object; title: string; discription: string }) => {
    return (
      <div className={style.mywork}>
-        <div className={style.preview}>
-           <a href=" "><img src=" " alt="img"/></a>
+        <div className={style.preview} style={props.style}>
+           <a className={style.viewButton}>look</a>
+          
         </div>
-        <h3>{props.title}</h3>
-        <div className={style.description}>{props.discription}</div>
-     
+         <div className={style.description}>
+             <h3>{props.title}</h3>
+             <div>{props.discription}</div>
+         </div>
      
      </div>
    );
--- src/MyWorks/Works.tsx
+++ src/MyWorks/Works.tsx
@@ -2,16 +2,30 @@ import React from 'react';
 import style from "./works.module.css"
 import OneWork from "./Work/OneWork";
 import Title from "../common/Components/Title";
+import toDoImage from "../../src/assets/image/td.png"
+import socNetImage from "../../src/assets/image/ss.png"
 
 const Works = () => {
+    const socNetworkStyle = {
+        backgroundImage: `url(${socNetImage})`,
+
+    }
+    const toDoListStyle = {
+        backgroundImage:`url(${toDoImage})`,
+
+    }
+    
+    
     return (
         <div className={style.worksBlock}>
             <div className={style.worksConteiner}>
                 <Title title={"my works"}/>
                 <div className={style.works}>
-                    <OneWork title={"Social network"}
+                    <OneWork style={socNetworkStyle}
+                             title={"Social network"}
                              discription={"Lorem ipsum dolor labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Co"}/>
-                    <OneWork title={"Todo List"}
+                    <OneWork style={toDoListStyle}
+                             title={"Todo List"}
                              discription={", consectetur adipis et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Vo"}/>
                 
                 </div>
