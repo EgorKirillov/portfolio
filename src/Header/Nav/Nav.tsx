2a488eb255ed2a3776d9780aa68c366100d1b9c4
EgorKirillov <80296253508@mail.ru>
2022-08-16 20:00:12 +0300

    change header

--- src/Header/Nav/Nav.tsx
+++ src/Header/Nav/Nav.tsx
@@ -4,10 +4,10 @@ import style from "./Nav.module.css"
 const Nav = () => {
    return (
      <div className={style.nav}>
-       <a href={"#mainBlock"}>Главная</a>
-       <a href={"#skills"}>Скиллы</a>
-       <a href={"#myWorks"}>Проекты</a>
-       <a href={"#contacts"}>Контакты</a>
+       <a href={"#mainBlock"}>Main</a>
+       <a href={"#skills"}>Skills</a>
+       <a href={"#myWorks"}>My Project</a>
+       <a href={"#contacts"}>Contacts</a>
        </div>
    );
 };
