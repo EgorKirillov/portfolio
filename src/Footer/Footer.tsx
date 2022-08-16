eb192bf43133877a745bde88cc64c544f08b22f0
EgorKirillov <80296253508@mail.ru>
2022-08-16 19:56:33 +0300

    add Footer icons and links (add somelinks)

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import style from "./Footer.module.scss"
 import linledInIcon from "./../assets/image/LinkIcons/linkedin-svgrepo-com-black.svg"
-import codewarsIcon from "./../assets/image/LinkIcons/codewars-red.svg"
+import codewarsIcon from "./../assets/image/LinkIcons/codewars-svgrepo-com-black.svg"
 import githubIcon from "./../assets/image/LinkIcons/github-svgrepo-com-black.svg"
 import freecodecampIcon from "./../assets/image/LinkIcons/freecodecamp-svgrepo-com.svg"
+import telegramIcon from "./../assets/image/LinkIcons/telegram-svgrepo-com-blue.svg"
+import viberIcon from "./../assets/image/LinkIcons/viber-svgrepo-com.svg"
+import mailIcon from "./../assets/image/LinkIcons/mail-ru-svgrepo-com.svg"
 import {FooterLink} from "./FooterLink";
 
 const Footer = () => {
@@ -16,20 +19,33 @@ const Footer = () => {
                                 src={linledInIcon}
                                 name={"LinkedIn.com"}
                     />
-                    <FooterLink link={"https://www.codewars.com/users/EgorKirillov"}
-                                src={codewarsIcon}
-                                name={"codewars.com"}
+                    <FooterLink link={"https://t.me/EgorKirillov12"}
+                                src={telegramIcon}
+                                name={"telegram"}
+                    />
+                    <FooterLink link={"viber://chat?number=%2B375296253508"}
+                                src={viberIcon}
+                                name={"viber +375 29 625-35-08 "}
                     />
                     <FooterLink link={"https://github.com/EgorKirillov"}
                                 src={githubIcon}
                                 name={"github.com"}
                     />
+                    <FooterLink link={"https://www.codewars.com/users/EgorKirillov"}
+                                src={codewarsIcon}
+                                name={"codewars.com"}
+                    />
                     <FooterLink link={"https://www.freecodecamp.org/EgorKirillov"}
                                 src={freecodecampIcon}
-                                name={"github.com"}
+                                name={"freecodecamp.com"}
+                    />
+                    <FooterLink link={"mailto:80296253508@mail.ru"}
+                                src={mailIcon}
+                                name={"80296253508@mail.ru"}
                     />
                 </div>
-                <div className={style.endText}>© 2022 не рожден еще человек, который не смог бы стать программистом ©</div>
+                <div className={style.endText}>© 2022 не рожден еще человек, который не смог бы стать программистом ©
+                </div>
             
             </div>
         </div>
